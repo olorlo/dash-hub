dc8255ea9dd653ae1d91452fff6f2934dfd8597b
이영은 <yongeuni22@gmail.com>
2026-02-06 11:35:47 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB - DashHub

--- BOJ/Bronze/2750. 수 정렬하기/Main.cc
+++ BOJ/Bronze/2750. 수 정렬하기/Main.cc
@@ -5,36 +5,38 @@
 int main() {
 
 	int N = 0;
+	int offset = 1000;
 	scanf("%d", &N);
 	int* arr = (int*)malloc(sizeof(int)*N);
 	// 입력
 	for (int i = 0; i < N; i++)
 		scanf("%d", &arr[i]);
 	
-	// cnt 인덱스를 위해서 arr 최대값 찾기
-	int max_arr = 0;
-	for (int i = 0; i < N; i++)
-		if (arr[i] > max_arr) {
-			max_arr = arr[i];
-		}
+	// cnt 인덱스를 위해서 arr 최대값 찾기 (필요없다..)
+	//int max_arr = 0;
+	//for (int i = 0; i < N; i++)
+	//	if (arr[i] > max_arr) {
+	//		max_arr = arr[i];
+	//	}
 
 	// 카운팅 정렬
 	// calloc으로 동적할당하면 0으로 자동 초기화
-	int* cnt = (int*)calloc(max_arr+1,sizeof(int));
+	int* cnt = (int*)calloc(2001, sizeof(int));
+	//int* cnt = (int*)malloc(sizeof(int)*max_arr + 1);
 	int* temp = (int*)malloc(sizeof(int)*N);
 
 	// 발생 횟수 기록
 	for (int i = 0; i < N; i++)
-		cnt[arr[i]] += 1;
+		cnt[arr[i]+offset] += 1;
 
-	// 누적 카운티 배열
-	for (int i = 1; i < max_arr + 1; i++)
+	// 누적 카운팅 배열
+	for (int i = 1; i < 2001; i++)
 		cnt[i] += cnt[i - 1];
 
 	// cnt 값 1씩 빼고, 뺸 값을 temp 인덱스로 접근해서 arr 값 삽입
 	for (int i = N - 1; i > -1; i--) {
-		cnt[arr[i]] -= 1;
-		temp[cnt[arr[i]]] = arr[i];
+		cnt[arr[i]+offset] -= 1;
+		temp[cnt[arr[i]+offset]] = arr[i];
 	}
 
 	// 출력
