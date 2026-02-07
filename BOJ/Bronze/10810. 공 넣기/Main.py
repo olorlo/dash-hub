05778449b9d2833e64ae216ca89b3459d4a3df30
이영은 <yongeuni22@gmail.com>
2026-02-07 23:18:57 +0900

    [Bronze III] Title: 공 넣기, Time: 40 ms, Memory: 32544 KB - DashHub

--- BOJ/Bronze/10810. 공 넣기/Main.py
+++ BOJ/Bronze/10810. 공 넣기/Main.py
@@ -2,25 +2,16 @@
 # 공 넣기
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(M)]
-print(arr)
 
 box = {}
 for i in range(1,N+1):
     if box.get(i,None) == None:
         box[i]=0
 
-# arr[M][0,1,2]
 for m in range(M):
-    print('arr 행: ',m)
-    # for i in range(m+1):
     for j in range(arr[m][0],arr[m][1]+1):
-        print('j: ',j)
-
         if box.get(j,None) == None:
             box[j]=0
-
         box[j]=arr[m][2]
-        print('box: ',box)
-
 
 print(*list(box.values()))
