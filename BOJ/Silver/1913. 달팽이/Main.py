b4d274e4921a4b02928d8bfe5518377db85c2b17
이영은 <yongeuni22@gmail.com>
2026-02-08 02:19:49 +0900

    [Silver III] Title: 달팽이, Time: 1112 ms, Memory: 70928 KB - DashHub

--- BOJ/Silver/1913. 달팽이/Main.py
+++ BOJ/Silver/1913. 달팽이/Main.py
@@ -1,14 +1,20 @@
 # 백준 1913번
 # 달팽이
 N =int(input())
+
 find = int(input())
-arr = [[0]*N for _ in range(N)]
-current= N*N #현재 값
-direction = 0 # 현재 바라보는 방향
-x, y = 0 ,0
+# 좌표 찾을 때 좌표 저장할 변수
 x_index = 0
 y_index = 0
-print(arr)
+
+arr = [[0]*N for _ in range(N)]
+
+current= N*N #현재 값
+direction = 0 # 현재 바라보는 방향
+
+# 현재 좌표
+x, y = 0 ,0
+
 
 # 하 우 상 좌
 dy = [1, 0, -1, 0]
@@ -23,7 +29,7 @@ while 0<current <= N*N:
     nx = x + dx[direction]
 
     # 뱡향 전환(다음 좌표가 범위 밖 or 이미 데이터 존재)
-    if ny < 0 or ny >=N or nx <0 or nx >=N or arr[ny][nx]!=0:
+    if ny < 0 or ny >=N or nx <0 or nx >=N or arr[ny][nx]:
         direction = (direction + 1) % 4
 
     current -= 1
