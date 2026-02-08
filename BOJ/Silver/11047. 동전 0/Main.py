ce13c87383840e280d9c505b1d0bae4bf65e4703
이영은 <yongeuni22@gmail.com>
2026-02-08 23:47:50 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB - DashHub

--- BOJ/Silver/11047. 동전 0/Main.py
+++ BOJ/Silver/11047. 동전 0/Main.py
@@ -3,19 +3,11 @@
 
 N, K = map(int, input().split())
 arr = [int(input()) for _ in range(N)]
-a =0
 cnt = 0
+arr = list(reversed(arr))
 
-while K > 0:
-    # arr 값 중 K보다 작지만 그중 최대 값 a
-    if K in arr:
-        cnt += 1
-        break
-    for i in arr:
-        if i < K:
-            a=i
-    print('a: ', a)
-    cnt += K // a
-    K = K % a
+for i in arr:
+    cnt += K // i
+    K %= i
 
 print(cnt)
