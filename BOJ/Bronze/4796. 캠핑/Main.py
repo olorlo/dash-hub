2eedb7325ccfb08fd254f398adf6f1f0410d7202
이영은 <yongeuni22@gmail.com>
2026-02-08 17:00:39 +0900

    [Bronze I] Title: 캠핑, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Bronze/4796. 캠핑/Main.py
+++ BOJ/Bronze/4796. 캠핑/Main.py
@@ -7,12 +7,5 @@ while(True):
     L, P, V =map(int,input().split())
     if L==0 and P ==0 and V ==0:
         break
-    cnt=1
-    # cnt 세기
-    for i in range(1,V):
-        for k in range(1,V//P+1):
-            if k*P -2 <= i <= k*P:
-                cnt-=1
-        cnt+=1
     # 출력
-    print(f'Case {tc}: {cnt}')
+    print(f'Case {tc}: {V-(V//P)*(P-L)}')
