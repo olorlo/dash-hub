421d4128c5c3d4b112e7b1a75a3715b8831ba3b9
이영은 <yongeuni22@gmail.com>
2026-02-08 16:29:25 +0900

    [Bronze I] Title: 캠핑, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Bronze/4796. 캠핑/Main.py
+++ BOJ/Bronze/4796. 캠핑/Main.py
@@ -8,14 +8,13 @@ while(True):
     if L==0 and P ==0 and V ==0:
         break
     arr = [0] * (V+1)
-    cnt=0
+    cnt=1
     # 리스트에 넣기
-    for i in range(len(arr)):
+    for i in range(1,len(arr)):
         for k in range(1,V//P+1):
             if k*P -2 <= i <= k*P:
                 arr[i] = 0
                 cnt-=1
-                break
         arr[i]=cnt
         cnt+=1
 
