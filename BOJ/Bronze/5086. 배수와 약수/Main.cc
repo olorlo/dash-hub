3358d74d2f3bfadeb0d78620fc70021757235661
이영은 <yongeuni22@gmail.com>
2026-02-10 23:02:00 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB - DashHub

--- BOJ/Bronze/5086. 배수와 약수/Main.cc
+++ BOJ/Bronze/5086. 배수와 약수/Main.cc
@@ -1,3 +1,5 @@
+// 백준 5086번 배수와 약수
+
 #include <iostream>
 using namespace std;
 
@@ -5,13 +7,14 @@ int main() {
 	int a, b;
 	while (true) {
 		cin >> a >> b;
-		if (a == 0 and b == 0)
+		if (a == 0 && b == 0)
 			break;
+		// 줄바꿈 방식 2가지 \n, endl
 		if (b % a == 0)
-			cout << "factor";
+			cout << "factor\n";
 		else if (a % b == 0)
-			cout << "multiple";
+			cout << "multiple" << endl;
 		else
-			cout << "neither";
+			cout << "neither" << endl;
 	}
 }
