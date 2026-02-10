8a93f99f05e74acbfa3ea9b6bb56bc5dced5c9ed
이영은 <yongeuni22@gmail.com>
2026-02-10 22:57:23 +0900

    [Bronze III] Title: 배수와 약수, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Bronze/5086. 배수와 약수/Main.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	while (true) {
+		cin >> a >> b;
+		if (a == 0 and b == 0)
+			break;
+		if (b % a == 0)
+			cout << "factor";
+		else if (a % b == 0)
+			cout << "multiple";
+		else
+			cout << "neither";
+	}
+}
