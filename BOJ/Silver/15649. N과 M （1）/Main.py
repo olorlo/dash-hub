71fa3fb9c48c76d2e872248f6da5d9dc61100685
이영은 <yongeuni22@gmail.com>
2026-02-11 17:34:48 +0900

    [Silver III] Title: N과 M (1), Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Silver/15649. N과 M （1）/Main.py
@@ -0,0 +1,18 @@
+# 백준 15649번 N, M (1)
+
+def bt(path):
+    if len(path) == M:
+        print(*path)
+        return 
+    for i in range(1, N+1):
+        if visited[i] == 0:
+            visited[i] == 1
+            path.append(i)
+            bt(path)
+            path.pop()
+            visited[i] = 0    
+
+N, M = map(int, input().split())
+
+visited = [0] * (N+1)
+bt([])
