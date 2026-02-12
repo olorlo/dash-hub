226837f39276ffd7771043ddce23661971860ac3
이영은 <yongeuni22@gmail.com>
2026-02-12 15:16:57 +0900

    [Silver III] Title: 주유소, Time: 68 ms, Memory: 42660 KB, Score: 58 point - DashHub

--- BOJ/Silver/13305. 주유소/Main.py
+++ BOJ/Silver/13305. 주유소/Main.py
@@ -3,16 +3,23 @@
 N = int(input())
 road_len = list(map(int, input().split()))
 station = list(map(int, input().split()))
+min_fee =10000
 fee = 0
 i = 0
-while True:
-    if station[i] >= station[i+1]:
-        fee += station[i] * road_len[i]
-        i+=1
-    else:
-        fee += station[i] * (road_len[i]+road_len[i+1]) 
-        i += 2
-    if i >= len(road_len):
-        break
-    
+
+# while True: (틀린 코드)
+    # if station[i] >= station[i+1]:
+    #     fee += station[i] * road_len[i]
+    #     i+=2
+    # else:
+    #     fee += station[i] * (road_len[i]+road_len[i+1]) 
+    #     i += 2
+    # if i >= len(road_len):
+    #     break
+
+for i in range(len(road_len)):
+    if station[i] <= min_fee:
+        min_fee = station[i] 
+    fee += min_fee * road_len[i]
+
 print(fee)
