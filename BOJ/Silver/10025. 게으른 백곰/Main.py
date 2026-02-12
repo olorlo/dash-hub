1affa6e36a0da3b5cdbfc8907abdaba4fba82e02
이영은 <yongeuni22@gmail.com>
2026-02-12 17:03:49 +0900

    [Silver III] Title: 게으른 백곰, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Silver/10025. 게으른 백곰/Main.py
+++ BOJ/Silver/10025. 게으른 백곰/Main.py
@@ -1,18 +1,22 @@
 # 백준 10025번 게으른 백곰
+MAX = 1000000
 N, K = map(int,input().split())
 ice = [list(map(int, input().split())) for _ in range(N)]
 
-arr = [0] * 1000001
+arr = [0] * (MAX + 1)
 for i in range(N):
     arr[ice[i][1]]+=ice[i][0]
 
-temp =0
+if 2 * K >= MAX:
+    max_ice = sum(arr)
+
 # 초기 상태 지정
+temp =0
 for i in range(2 * K + 1):
     temp += arr[i]
 
 max_ice = temp
-for i in range(1000000 - 2 * K):
+for i in range(MAX - 2 * K):
     temp = temp - arr[i] + arr[i + 2 * K+1] 
 
     max_ice = max(temp, max_ice)
