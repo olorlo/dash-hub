f3a9612b9f131ff7b00b85e35f0d51c83d5938e0
이영은 <yongeuni22@gmail.com>
2026-02-13 15:10:57 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 32412 KB - DashHub

--- BOJ/Silver/1541. 잃어버린 괄호/Main.py
+++ BOJ/Silver/1541. 잃어버린 괄호/Main.py
@@ -1,3 +1,4 @@
+
 # 백준 1541번 잃어버린 괄호
 
 # # 수식을 기준으로 나누는 방법
@@ -6,8 +7,7 @@
 
 s = input().split('-')
 result = sum(map(int, s[0].split('+')))
-
 for i in s[1:]:
-    result -= sum(map(int, i[0].split('+')))
+    result -= sum(map(int, i.split('+')))
 
 print(result)
