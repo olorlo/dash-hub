fa687f87c01cf737f3435885e70ae599ea808d9d
이영은 <yongeuni22@gmail.com>
2026-02-19 16:12:25 +0900

    [Silver IV] Title: 요세푸스 문제, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Silver/1158. 요세푸스 문제/Main.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+# 백준 1158번 요세푸스 문제
+N, K = map(int, input().split())
+
+queue = deque()
+
+for i in range(1, N + 1):
+    queue.append(i)
+
+print('<', end ='')
+while len(queue)>1:
+    queue.append(queue.popleft())
+    queue.append(queue.popleft())
+    
+    print(f'{queue.popleft()}', end = ', ')
+print(queue.popleft(), end='')
+print('>')
