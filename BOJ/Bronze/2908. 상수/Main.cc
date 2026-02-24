dfe44e2485c66786346ddb3f09d92e34eb226ecb
이영은 <yongeuni22@gmail.com>
2026-02-25 00:36:50 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB - DashHub

---
+++ BOJ/Bronze/2908. 상수/Main.cc
@@ -0,0 +1,22 @@
+// 백준 2908번 C++ 상수
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	string A, B;
+	cin >> A >> B;
+
+	reverse(A.begin(), A.end());
+	reverse(B.begin(), B.end());
+
+	if (A > B) {
+		cout << A;
+	}
+	else {
+		cout << B;
+	}
+
+}
