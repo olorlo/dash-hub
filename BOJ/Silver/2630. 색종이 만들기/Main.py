fd0ae0cbd9658e3b56fcf40523c872d25abf98f3
이영은 <yongeuni22@gmail.com>
2026-02-25 20:47:24 +0900

    [Silver II] Title: 색종이 만들기, Time: 52 ms, Memory: 32412 KB - DashHub

--- BOJ/Silver/2630. 색종이 만들기/Main.py
+++ BOJ/Silver/2630. 색종이 만들기/Main.py
@@ -6,34 +6,34 @@ cnt_blue = 0
 cnt_white = 0
 
 def cut(start_row, end_row, start_col, end_col):
-    global cnt_blue
-    global cnt_white
-
-    idx = (end_row - start_row) + 1
+    global cnt_blue, cnt_white
     sum_arr = 0
     len_arr = 0
     for i in range(start_row,end_row+1):
         for j in range(start_col, end_col+1):
             sum_arr += arr[i][j]
             len_arr += 1
+
     if sum_arr == 0:
         cnt_white += 1
         return 
     elif sum_arr == len_arr:
         cnt_blue += 1
         return
+    
+    mid_row = (end_row + start_row) // 2
+    mid_col = (end_col + start_col) // 2
     # 왼쪽 위
-    cut(start_row, end_row//2, start_col, start_col + idx//4)
+    cut(start_row, mid_row, start_col, mid_col)
     # 오른쪽 위
-    cut(start_row, start_row + idx//4, end_col - idx//4, end_col)
+    cut(start_row, mid_row, mid_col+1, end_col)
     # 왼쪽 아래
-    cut(end_row-idx//4, end_row, start_col, start_col + idx//4)       
+    cut(mid_row+1, end_row, start_col, mid_col)       
     # 오른쪽 아래
-    cut(end_row-idx//4, end_row, end_col - idx//4, end_col)
+    cut(mid_row+1, end_row, mid_col+1, end_col)
 
     return cnt_white, cnt_blue
-    print('blue', cnt_blue)
-    return 
+
 cut(0, N - 1, 0, N - 1)    
 print(cnt_white)
 print(cnt_blue)
