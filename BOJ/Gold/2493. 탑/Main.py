69b2bf81392f3ed94f9c931690d1a19b53f128e9
이영은 <yongeuni22@gmail.com>
2026-02-27 01:31:17 +0900

    [Gold V] Title: 탑, Time: 596 ms, Memory: 132140 KB - DashHub

--- BOJ/Gold/2493. 탑/Main.py
+++ BOJ/Gold/2493. 탑/Main.py
@@ -3,24 +3,39 @@ import sys
 input = sys.stdin.readline
 
 # 백준 2493번 탑
+# 핵심: 왼쪽에서 자기보다 처음으로 큰 탑 찾기
+
 N = int(input())
-stack = list(map(int, input().split()))
+height = list(map(int, input().split()))
 
-# stack = []
-result = []
-while True:
-    a= stack.pop()
-    if not stack:
-        result.append(0)
-        break
-    if a > max(stack):
-        result.append(0)
-    else:
-        for i in range(len(stack)-1,-1,-1):
-            if a < stack[i]:
-                result.append(i+1)
-                break
+stack = []
+result = [0] * N
+# 시간초과 난 코드
+# while True:
+#     a= stack.pop()
+#     if not stack:
+#         result.append(0)
+#         break
+#     if a > max(stack):
+#         result.append(0)
+#     else:
+#         for i in range(len(stack)-1,-1,-1):
+#             if a < stack[i]:
+#                 result.append(i+1)
+#                 break
 
-print(*list(reversed(result)))
+for i in range(N):
+    h = height[i]
 
+    #현재보다 작은 탑 제거
+    while stack and stack[-1][1] < h: 
+        stack.pop()
 
+    # 남아있는게 있으면 그게 수신 탑
+    if stack:
+        result[i] = stack[-1][0] + 1
+
+    # 현재 탑 추가
+    stack.append((i,h))
+
+print(*result)
