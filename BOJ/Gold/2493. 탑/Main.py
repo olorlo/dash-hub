5538b0ee1c1f50b7301ad12ed7e20b830a9d9130
이영은 <yongeuni22@gmail.com>
2026-02-27 01:14:46 +0900

    [Gold V] Title: 탑, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Gold/2493. 탑/Main.py
@@ -0,0 +1,20 @@
+# 백준 2493번 탑
+N = int(input())
+stack = list(map(int, input().split()))
+
+# stack = []
+result = []
+while True:
+    a= stack.pop()
+    if not stack:
+        result.append(0)
+        break
+    if a > max(stack):
+        result.append(0)
+    else:
+        for i in range(len(stack)-1,-1,-1):
+            if a < stack[i]:
+                result.append(i+1)
+                break
+
+print(*list(reversed(result)))
