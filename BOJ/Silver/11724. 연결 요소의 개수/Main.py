444c987984b74b690c1b3278a015eefce082f5e1
이영은 <yongeuni22@gmail.com>
2026-02-27 12:08:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Silver/11724. 연결 요소의 개수/Main.py
+++ BOJ/Silver/11724. 연결 요소의 개수/Main.py
@@ -1,6 +1,5 @@
 # 백준 11724번 연결 요소의 개수
 N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(M)]
 graph = [[] for _ in range(N+1)]
 
 visited = [0] * (N+1)
@@ -9,14 +8,14 @@ cnt = 0
 def recur(v):
 
     visited[v] = 1
-
-    # 다음 연결된 정점으로 이동 
+    # 다음 연결된 정점으로 이동
     for next in graph[v]:
         if not visited[next]:
             recur(next)
 
 # 방향 없는 그래프 만들기 
-for u, v in arr:
+for _ in range(M):
+    u, v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
 
