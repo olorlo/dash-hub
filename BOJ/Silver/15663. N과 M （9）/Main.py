ec62a925d39fa8124521c8eef667823199b14b24
이영은 <yongeuni22@gmail.com>
2026-02-28 17:12:48 +0900

    [Silver II] Title: N과 M (9), Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Silver/15663. N과 M （9）/Main.py
@@ -0,0 +1,49 @@
+import sys
+sys.stdin = open("input.txt", 'r')
+
+# 백준 dfs일 경우 무조건 추가해준다.
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+# 백준 15663번 N과 M(9)
+def recur(path):
+
+    # 종료 조건
+    # 깊이: len(path)
+    if len(path) == M:
+        print(*path)
+        return 
+    
+    # prev: 현재 깊이에서 중복이 있는지 체크하기 위한 변수
+    # -> 깊이마다 따로 존재해야함
+    prev = -1
+
+    # 현재 깊이에서 선택할 수 있는 후보들 
+    for i in range(N):
+        # print(visited)
+        if visited[i]:
+            continue
+        
+        # 중복인지 검사
+        if arr[i] == prev:
+            continue
+       
+        visited[i] = 1
+        path.append(arr[i])
+
+        # 중복 제거
+        prev = arr[i]
+
+        recur(path)
+
+        path.pop()
+        visited[i] = 0
+
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+result = []
+visited = [0] * (N)
+
+recur([])
