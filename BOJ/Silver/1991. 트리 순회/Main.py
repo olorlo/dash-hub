3b684e9e2c112aa30bf0893358856e858beded82
이영은 <yongeuni22@gmail.com>
2026-03-02 00:26:52 +0900

    [Silver I] Title: 트리 순회, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Silver/1991. 트리 순회/Main.py
@@ -0,0 +1,44 @@
+import sys
+# sys.stdin = open("input.txt", 'r')
+input = sys.stdin.readline
+
+# 백준 1991번 트리 순회
+
+N = int(input())
+tree = {}
+
+for _ in range(N):
+    node, left, right = input().split()
+    tree[node] = (left, right)
+
+# 순회 결과를 저장할 리스트
+preorder = []
+inorder = []
+postorder = []
+
+
+def recur(node):
+    # 자식이 없는 경우: 리턴
+    if node == '.':
+        return 
+    
+    # 전위순회: 뿌리부터 왼쪽, 오른쪽
+    preorder.append(node)
+
+    # 왼쪽 자식으로 이동
+    recur(tree[node][0])
+
+    # 중위순회: 왼쪽, 뿌리, 오른쪽
+    inorder.append(node)
+
+    # 오른쪽 자식으로 이동
+    recur(tree[node][1])
+
+    # 후위순회: 왼쪽, 오른쪽, 뿌리
+    postorder.append(node)
+
+
+recur('A')
+print(*preorder)
+print(*inorder)
+print(*postorder)
