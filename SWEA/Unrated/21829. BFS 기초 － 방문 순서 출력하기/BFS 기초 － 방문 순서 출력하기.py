1598ecaa1cedb31d0c2e752ad4e79ebda772043f
이영은 <yongeuni22@gmail.com>
2026-03-03 00:17:44 +0900

    [Unrated] Title: BFS 기초 - 방문 순서 출력하기, Time: -1, Memory: -1 - DashHub

---
+++ SWEA/Unrated/21829. BFS 기초 － 방문 순서 출력하기/BFS 기초 － 방문 순서 출력하기.py
@@ -0,0 +1,38 @@
+
+from collections import deque
+
+# 백준 21829 bfs기초-방문순서출력하기
+T = int(input())
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+def bfs(node):
+    dq = deque([node])
+    visited[node] = 1
+
+    while dq:
+        now = dq.popleft()
+        print(now, end = ' ')
+        for next in graph[now]:
+            if visited[next]:
+                continue
+            visited[next] = 1
+            dq.append(next)      
+
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    graph = [[] for _ in range(N)]
+    visited = [0] * (N+1) 
+
+    # 인접 리스트로 만들기
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] == 1:
+                graph[i].append(j)
+
+    print(f'#{tc}', end= ' ')
+    for i in range(N):
+        if not visited[i] and graph[i]:
+            bfs(i)
+    print()
---
+++ SWEA/Unrated/21829. BFS 기초 － 방문 순서 출력하기/README.md
@@ -0,0 +1,15 @@
+# [Unrated] BFS 기초 - 방문 순서 출력하기 - 21829 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZFOgFAKwZcDFAVs) 
+
+### 성능 요약
+
+메모리: -1, 시간: -1, 코드길이: 848 Bytes
+
+### 제출 일자
+
+2026-03-03 00:17
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
