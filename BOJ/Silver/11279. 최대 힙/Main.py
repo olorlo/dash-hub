68a835ab9c075fab1664843fce522cc74c9b63a7
이영은 <yongeuni22@gmail.com>
2026-03-04 00:22:37 +0900

    [Silver II] Title: 최대 힙, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Silver/11279. 최대 힙/Main.py
@@ -0,0 +1,15 @@
+import heapq
+
+# 백준 11279번 최대 힙
+N = int(input())
+heap = []
+for _ in range(N):
+    x = int(input())
+    if x == 0 and heap:
+        # 최대값 출력하고, 그 값을 배열에서 제거함
+        print(-heapq.heappop(heap))
+    elif x == 0 and not heap:
+        print(0)
+    else:
+        # 최대 힙을 만들기 위해서 음수값을 넣는다.
+        heapq.heappush(heap, x * (-1))
