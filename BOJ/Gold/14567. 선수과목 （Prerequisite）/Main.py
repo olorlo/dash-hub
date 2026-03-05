f8d79715b3b35bd9d036a4cc64e5d375f43dc739
이영은 <yongeuni22@gmail.com>
2026-03-05 14:18:16 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 536 ms, Memory: 45148 KB - DashHub

--- BOJ/Gold/14567. 선수과목 （Prerequisite）/Main.py
+++ BOJ/Gold/14567. 선수과목 （Prerequisite）/Main.py
@@ -1,35 +1,27 @@
 import sys
-# from collections import deque
-# sys.stdin = open("python/input.txt", 'r')
+# sys.stdin = open("python/input.txt", "r")
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-# 백준 14567번 선수과목
 def dfs(node):
-    global cnt
-    if not graph[node]:
-        stack[node] = cnt + 1
-        return
-    visited[node] = 1
+    max_sem = 0
+    if visited[node]:
+        return visited[node]
+    max_sem = 0
     for next in graph[node]:
-        if visited[next]:
-            continue
-        cnt += 1
-        dfs(next)
-        stack[node] = cnt
-        cnt -= 1
-        visited[next] = 0
+        max_sem = max(max_sem, dfs(next))
+    visited[node] = max_sem + 1
+    return visited[node]
 
 N, M = map(int, input().split())
 graph = [[] for _ in range(N+1)]
-cnt = 0
+visited = [0]*(N+1)
 
 for _ in range(M):
     A, B = map(int, input().split())
-    graph[A].append(B)
+    graph[B].append(A)
 
-stack = [1] * (N+1)
-visited = [0] * (N+1)
+for i in range(N+1):
+    dfs(i)
 
-dfs(1)
-for i in range(1, N+1):
-    print(stack[i], end=' ')
+print(*visited[1:])
