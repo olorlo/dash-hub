0f554785a7923d444bbd04a5a67315520a6099a4
이영은 <yongeuni22@gmail.com>
2026-03-10 14:23:14 +0900

    [Gold IV] Title: 알파벳, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Gold/1987. 알파벳/Main.py
+++ BOJ/Gold/1987. 알파벳/Main.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10**6)
+# sys.setrecursionlimit(10**6)
 # sys.stdin = open("input.txt", "r")
 
 from collections import deque
@@ -19,29 +19,28 @@ def dfs(y, x, mask, cnt):
         return
 
     max_value = max(max_value, cnt)
-    
+
     for k in range(4):
         ny = y + dy[k]
         nx = x + dx[k]
 
-        if ny < 0 or ny >= R or nx < 0 or nx >= C:
-            continue
-        
-        # 시간 초과난 코드
-        # if arr[ny][nx] in stack:
-        #     continue
-        # -> bitmask로 수정
-        # 비트마스크란? 리스트 말고 하나의 문자로 방문했는지 안했는지 체크하는 것
+        if 0 <= ny < R and 0 <= nx < C:
+  
+            # 시간 초과난 코드
+            # if arr[ny][nx] in stack:
+            #     continue
+            # -> bitmask로 수정
+            # 비트마스크란? 리스트 말고 하나의 문자로 방문했는지 안했는지 체크하는 것
 
-        alpha = ord(arr[ny][nx]) - 65
+            alpha = ord(arr[ny][nx]) - 65
 
-        # 이미 방문한 곳이라면 지나감
-        # alpha 자리에 스위치가 켜져있고, mask 그렇다면 같은 곳을 방문한 것 -> 그냥 지나간다
-        if mask & (1 << alpha):
-            continue
+            # 이미 방문한 곳이라면 지나감
+            # alpha 자리에 스위치가 켜져있고, mask 그렇다면 같은 곳을 방문한 것 -> 그냥 지나간다
+            if mask & (1 << alpha):
+                continue
 
-        dfs(ny, nx, mask | (1 << alpha), cnt + 1)
- 
+            dfs(ny, nx, mask | (1 << alpha), cnt + 1)
+    
 
 max_value = 0
 start = ord(arr[0][0]) - 65
