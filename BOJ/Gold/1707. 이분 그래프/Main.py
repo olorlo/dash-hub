35ac35921a9512b0cadcf27c00a3d5523d8f46ce
이영은 <yongeuni22@gmail.com>
2026-03-10 15:44:46 +0900

    [Gold IV] Title: 이분 그래프, Time: 1484 ms, Memory: 52888 KB - DashHub

--- BOJ/Gold/1707. 이분 그래프/Main.py
+++ BOJ/Gold/1707. 이분 그래프/Main.py
@@ -1,8 +1,7 @@
 import sys
 # sys.setrecursionlimit(10**6)
 # sys.stdin = open("input.txt", "r")
-
-from collections import deque
+input = sys.stdin.readline
 
 # 백준 1707번 이분그래프
 
@@ -30,29 +29,19 @@ for _ in range(K):
 
     # 이분그래프 만들기
     for start in range(1, V+1):
+
         # 이미 start 정점이 집합에 들어있으면 패스
         if color[start] != 0:
             continue
         
         stack = [start]
-        # 집합 1에 넣었다고 생각
         color[start] = 1
-        # set1.add(start)
 
-        # stack이 빌때까지 반복
         while stack:
             i = stack.pop()
 
             # 현재 정점과 연결된 정점
             for next in graph[i]:
-
-                # 이 그래프가 충돌인지 확인 -> 집합으로 했는데 시간초과남
-                # if i in set1 and next in set1:
-                #     is_bipartite = False
-                #     break
-                # if i in set2 and next in set2:
-                #     is_bipartite = False
-                #     break
                 
                 if color[i] == color[next]:
                     is_bipartite = False
@@ -60,7 +49,7 @@ for _ in range(K):
 
                 # 이미 집합 속에 들어있는지 확인 (중복 방지)
                 if color[next] == 0:
-                    color[next] -= color[i]
+                    color[next] = -color[i]
                     stack.append(next)
 
             if not is_bipartite:
