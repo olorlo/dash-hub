6ccfbcaaf9b6b193d30869e8096ce35fc69d3ad8
이영은 <yongeuni22@gmail.com>
2026-03-10 17:02:25 +0900

    [Gold III] Title: 텀 프로젝트, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Gold/9466. 텀 프로젝트/Main.py
+++ BOJ/Gold/9466. 텀 프로젝트/Main.py
@@ -2,43 +2,47 @@ import sys
 # sys.setrecursionlimit(10**6)
 # sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
-
-from collections import deque
-
 # 백준 9466번 텀 프로젝트
+
 T = int(input())
 
 def dfs(node):
-    visited = [0] * (n+1)
-    stack.append(node)
+    global result
 
-    while stack:
-        now = stack.pop()
-        
-        next=students[now]
+    # 방문 처리 후 사이클에 해당 노드 추가
+    visited[node] = 1
+    cycle.append(node)
+    
+    # 현재와 연결되어있는 next 찾음
+    next = students[node]
 
-        if visited[next]:
-            return now
+    # 만약 next가 방문되지 않은 새 정점이라면 dfs 돌린다
+    if not visited[next]:
+        dfs(next)
 
-        visited[next] = 1
-        stack.append(next)
-
+    # 만약 cycle 돌아서 돌아왔다면(방문 되었다면)
+    # 사이클 내에 next가 있는지 확인 -> 사이클인지 확인
+    # 사이클이면   
+    # 1. 사이클 시작 위치 찾기
+    # 2. 그 위치부터 사이클 끝까지 잘라서 사이클 길이 재기
+    
+    else:
+        if next in cycle:
+            start = cycle.index(next)
+            team = cycle[start:]
+            result += len(team)
 
 for _ in range(T):
     n = int(input())
+    students = [0] + list(map(int, input().split()))
 
-    a = list(map(int, input().split()))
     visited = [0] * (n+1)
-    students = [0, *a]
+    result = 0
+    
+    # cycle 수를 세고 전체에서 빼기
+    for i in range(1,n+1):
+        if not visited[i]:
+            cycle = []
+            dfs(i)
 
-    # dfs 돌려서 내 자신이 나오면 팀에 속한거임
-    stack = []
-    cnt = 0
-
-    for start in range(1,n+1):
-        if dfs(start) == start:
-            continue
-        else:
-            cnt += 1
-
-    print(cnt)
+    print(n-result)
