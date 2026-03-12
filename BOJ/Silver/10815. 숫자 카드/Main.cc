e9ccd7c14710d8a365fa958dcaef1b187681e58e
이영은 <yongeuni22@gmail.com>
2026-03-12 23:28:22 +0900

    [Silver V] Title: 숫자 카드, Time: 560 ms, Memory: 7828 KB - DashHub

--- BOJ/Silver/10815. 숫자 카드/Main.cc
+++ BOJ/Silver/10815. 숫자 카드/Main.cc
@@ -1,10 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
 
 #include <iostream>
-#include <cstdio>
 #include <algorithm>
 #include <vector>
-#include <string>
 using namespace std;
 
 // 백준 C++ 10815번 숫자카드
@@ -12,12 +11,17 @@ int main() {
 	// 테케 자동 입력
 	// freopen("input.txt", "r", stdin);
 
+	// 범위가 너무 커서 완탐하면 시간 초과난다
+	// 이진 탐색으로 구현! -> C++: 이진탐색 함수 존재
 	int N;
 	cin >> N;
 
 	vector<int>arr(N);
 	for (int i = 0; i < N; i++)
 		cin >> arr[i];
+
+	// 이진탐색을 위해 arr 정렬
+	sort(arr.begin(), arr.end());
 
 	int M;
 	cin >> M;
@@ -29,18 +33,8 @@ int main() {
 	vector<int>result(M);
 
 	for (int i = 0; i < M; i++) {
-		for (int j = 0; j < N; j++) {
-			result[i] = 0;
-			if (b[i] == arr[j]) {
-				result[i] = 1;
-				break;
-			}
-
-		}
+		// binary_search 함수: 값이 존재하면 true, 없으면 false를 반환하는 함수
+		// binary_search(시작 iterator, 끝 iterator, 찾을 값)
+		cout << binary_search(arr.begin(), arr.end(), b[i]) << " ";
 	}
-	for (int i = 0; i < M; i++) {
-		cout << result[i] << " ";
-
-	}
-
 }	
