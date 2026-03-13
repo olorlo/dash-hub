5227d5f40566916f049704cd2f42e63733c752a0
이영은 <yongeuni22@gmail.com>
2026-03-13 12:19:35 +0900

    [Silver III] Title: 2×n 타일링, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Silver/11726. 2×n 타일링/Main.py
@@ -0,0 +1,16 @@
+input = sys.stdin.readline 
+
+# 백준 11726번 2xn 타일링
+# 사실은 피보나치 수열이엇던 것이엇던 것이엇다. 
+
+n = int(input())
+dp = [0]*(1+n)
+
+# 초기값 설정
+dp[1] = 1
+dp[2] = 2 
+
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
