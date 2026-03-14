238cb35aaeb798a3a08ea2b6c208706e8351d057
이영은 <yongeuni22@gmail.com>
2026-03-15 02:37:41 +0900

    [Bronze I] Title: 진법 변환 2, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Bronze/11005. 진법 변환 2/Main.py
@@ -0,0 +1,20 @@
+# 백준 11005번 진법 변환2
+N, B = map(int, input().split())
+result = ''
+stack = []
+
+while True:
+    stack.append(N % B)
+    if N < B :
+        break
+    N = N//B
+
+while stack:
+    s = stack.pop()
+    if 0 <= s <=9:
+        value = s
+    else:
+        value = chr(s-10+65)
+    result += value
+
+print(result)
