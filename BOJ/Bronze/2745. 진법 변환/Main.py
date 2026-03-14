1f4a0b00c2853453e2bd4393f113708f4f2a3d7f
이영은 <yongeuni22@gmail.com>
2026-03-15 02:24:50 +0900

    [Bronze II] Title: 진법 변환, Time: 32 ms, Memory: 32544 KB - DashHub

--- BOJ/Bronze/2745. 진법 변환/Main.py
+++ BOJ/Bronze/2745. 진법 변환/Main.py
@@ -1,13 +1,13 @@
 # 백준 2745번 진법 변환
 N, B = input().split()
-change = []
-for i in range(26):
-    change.append({chr(i+65):10+i})
-
+B = int(B)
 result = 0 
 
 for i in range(len(N)):
-    for j in range(26):
-        if N[i] == chr(j+65):
-            result += change[j][N[i]] * (int(B) ** (len(N) - 1- i))
+    if '0' <= N[i] <='9':
+        value = int(N[i])
+    else:
+        value = ord(N[i]) - ord('A') + 10
+    result += value * (B ** (len(N) - 1 -i))
+
 print(result)
