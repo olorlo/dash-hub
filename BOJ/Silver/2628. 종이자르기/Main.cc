d58fd2ffaf9f9b0280bf523184cee0377fbe778f
이영은 <yongeuni22@gmail.com>
2026-03-17 23:58:07 +0900

    [Silver V] Title: 종이자르기, Time: 0 ms, Memory: 2024 KB - DashHub

--- BOJ/Silver/2628. 종이자르기/Main.cc
+++ BOJ/Silver/2628. 종이자르기/Main.cc
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
 
 #include <iostream>
 #include <algorithm>
@@ -18,31 +19,39 @@ int main() {
 	cin >> a;
 
 	int b, c;
-	int max_y = 0;
-	int max_x = 0;
-	int y_border = m;
-	int x_border = n;
+
+	vector<int>y_arr;
+	vector<int>x_arr;
+
+	y_arr.push_back(0);
+	y_arr.push_back(m);
+
+	x_arr.push_back(0);
+	x_arr.push_back(n);
 
 	for (int cut = 0; cut < a; cut++) {
 		cin >> b >> c;
 
 		// 가로로 자르기
-		if (b == 0) {
-			if (c <= y_border / 2) 
-				max_y = max(max_y, y_border - c);
-			else 
-				max_y = max(max_y, c);
-			y_border = max_y;
-		}
-		// 세로로 자르기 ( b==1)
-		else if (b==1) {
-			if (c <= x_border / 2) 
-				max_x = max(max_x, n - c);
-			else 
-				max_x = max(max_x, c);
-			x_border = max_x;
-		}
+		if (b == 0) y_arr.push_back(c);
 
+		// 세로로 자르기 ( b==1)
+		else x_arr.push_back(c);
 	}		
+
+	sort(y_arr.begin(), y_arr.end());
+	sort(x_arr.begin(), x_arr.end());
+
+	int max_y = 0;
+	int max_x = 0;
+
+	for (int i = 1; i < y_arr.size(); i++) {
+		max_y = max(y_arr[i] - y_arr[i-1], max_y);
+	}
+
+	for (int i = 1; i < x_arr.size(); i++) {
+		max_x = max(x_arr[i] - x_arr[i-1], max_x);
+	}
+
 	cout << max_y * max_x;
 }	
