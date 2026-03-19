153e184140a0830a7b4b838aa3085153459877b5
이영은 <yongeuni22@gmail.com>
2026-03-20 02:26:57 +0900

    [Silver V] Title: 집합, Time: 624 ms, Memory: 2024 KB - DashHub

--- BOJ/Silver/11723. 집합/Main.cc
+++ BOJ/Silver/11723. 집합/Main.cc
@@ -11,44 +11,50 @@ using namespace std;
 // 백준 C++ 11723 집합
 
 int main() {
-	//freopen("input.txt", "r", stdin);
+
+	// 자동 입력
+	// freopen("input.txt", "r", stdin);
+
+	// 코테 기본 템플릿 : 빠른 입출력
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
 	int M;
 	cin >> M;
 
-	set<int>S;
+	// 비트마스크 풀이
+	int S = 0;
 
-	string a;
-	int b;
+	string cmd;
+	int x;
 
-	for (int i = 0; i < M; i++) {
-		cin >> a;
-		if (a == "all") {
-			S.clear();
-			for (int j = 0; j < 20; j++)
-				S.insert(j + 1);
-
+	while (M--) {
+		cin >> cmd;
+		if (cmd == "all") {
+			S = (1 << 20) - 1;
+			continue;
 		}
-		else if (a == "empty")
-			S.clear();
-		else cin >> b;
+		else if (cmd == "empty") {
+			S = 0;
+			continue;
+		}
 
-		if (a == "add")
-			S.insert(b);
-		else if (a == "remove")
-			S.erase(b);
-		else if (a == "check") {
-			if (S.find(b) != S.end())
+		cin >> x;
+		if (cmd == "add")
+			S |= (1 << (x - 1));
+		else if (cmd == "remove")
+			// x-1 자리에 1을 넣고 비트 반전시킴 + and 연산 수행하면 해당 자리만 비트 연산수행됨
+			S &= ~(1 << (x - 1));
+		else if (cmd == "check") {
+			if (S & (1 << (x - 1)))
 				cout << 1 << '\n';
 			else
 				cout << 0 << '\n';
 		}
-
-		else if (a == "toggle") {
-			if (S.find(b) != S.end())
-				S.erase(b);
-			else
-				S.insert(b);
-		}		
+		else if (cmd == "toggle") {
+			// S에 있으면 제거, 없으면 추가
+			S ^= (1 << (x - 1));
+		}
 	}
+
 }
