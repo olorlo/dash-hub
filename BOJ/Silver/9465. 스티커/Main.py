678d4434a08fd6586502b55f9056632b4ef0e4fc
이영은 <yongeuni22@gmail.com>
2026-03-21 01:37:14 +0900

    [Silver I] Title: 스티커, Time: 652 ms, Memory: 50760 KB - DashHub

--- BOJ/Silver/9465. 스티커/Main.py
+++ BOJ/Silver/9465. 스티커/Main.py
@@ -1,5 +1,6 @@
 import sys
 # sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
 
 from collections import deque
 
@@ -12,6 +13,10 @@ for tc in range(1, T+1):
 
     arr = [list(map(int, input().split())) for _ in range(2)]
 
+    if n == 1:
+        print(max(arr[0][0], arr[1][0]))
+        continue
+
     dp = [[0]*n for _ in range(2)]
 
     dp[0][0] = arr[0][0]
@@ -20,9 +25,10 @@ for tc in range(1, T+1):
     dp[0][1] = arr[0][1] + dp[1][0]
     dp[1][1] = arr[1][1] + dp[0][0]
 
+
     for i in range(2, n):
         dp[0][i] = max(dp[1][i-1], dp[1][i-2]) + arr[0][i]
         dp[1][i] = max(dp[0][i-1], dp[0][i-2]) + arr[1][i]
 
     print(max(dp[0][n-1], dp[1][n-1]))
-   
+    
