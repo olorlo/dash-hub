6fce4247386730ff9eeb7f9dddb9d4bab5e1e81e
이영은 <yongeuni22@gmail.com>
2026-03-21 20:34:16 +0900

    [Silver III] Title: 자리배정, Time: -1 ms, Memory: -1 KB - DashHub

---
+++ BOJ/Silver/10157. 자리배정/Main.cc
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+// 백준 C++ 10157 자리배정
+
+int main() {
+
+	// 자동 입력
+	// freopen("input.txt", "r", stdin);
+
+	// 코테 기본 템플릿 : 빠른 입출력
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int C, R;
+	cin >> C >> R;
+
+	int K;
+	cin >> K;
+
+	vector<vector<int>>arr(C, vector<int>(R, 0));
+
+	// 방향 배열(상우하좌)
+	int dy[4] = { -1, 0, 1, 0 };
+	int dx[4] = { 0, 1, 0, -1 };
+
+	// 시작
+	int start_y = C-1;
+	int start_x = 0;
+	int dir = 0;
+
+	int current = 1;
+
+	if (K > R * C) {
+		cout << 0;
+		return 0;
+	}
+
+	while (true) {
+
+
+		if (current == K) {
+			cout << start_x+1 << ' ' << start_y+1;
+			return 0;
+		}
+		arr[start_y][start_x] = current;
+
+		int ny = start_y + dy[dir];
+		int nx = start_x + dx[dir];
+
+		if (ny < 0 || ny >= C || nx < 0 || nx >= R || arr[ny][nx] != 0) {
+			dir = (dir + 1) % 4;
+		}
+
+		current += 1;
+		start_y += dy[dir];
+		start_x += dx[dir];
+
+	}
+	cout << 0;
+}
