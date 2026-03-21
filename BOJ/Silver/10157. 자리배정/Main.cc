4146cd2e25a882ff8f10f0ea8b4ad2b5eba223ea
이영은 <yongeuni22@gmail.com>
2026-03-21 20:36:16 +0900

    [Silver III] Title: 자리배정, Time: 4 ms, Memory: 5864 KB - DashHub

--- BOJ/Silver/10157. 자리배정/Main.cc
+++ BOJ/Silver/10157. 자리배정/Main.cc
@@ -24,14 +24,14 @@ int main() {
 	int K;
 	cin >> K;
 
-	vector<vector<int>>arr(C, vector<int>(R, 0));
+	vector<vector<int>>arr(R, vector<int>(C, 0));
 
 	// 방향 배열(상우하좌)
 	int dy[4] = { -1, 0, 1, 0 };
 	int dx[4] = { 0, 1, 0, -1 };
 
 	// 시작
-	int start_y = C-1;
+	int start_y = R-1;
 	int start_x = 0;
 	int dir = 0;
 
@@ -46,7 +46,7 @@ int main() {
 
 
 		if (current == K) {
-			cout << start_x+1 << ' ' << start_y+1;
+			cout << start_x+1 << ' ' << R-start_y;
 			return 0;
 		}
 		arr[start_y][start_x] = current;
@@ -54,7 +54,7 @@ int main() {
 		int ny = start_y + dy[dir];
 		int nx = start_x + dx[dir];
 
-		if (ny < 0 || ny >= C || nx < 0 || nx >= R || arr[ny][nx] != 0) {
+		if (ny < 0 || ny >= R || nx < 0 || nx >= C || arr[ny][nx] != 0) {
 			dir = (dir + 1) % 4;
 		}
 
