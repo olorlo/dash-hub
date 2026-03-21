ffd17ddccf2f1adcd3830429fb8975af287f01e1
이영은 <yongeuni22@gmail.com>
2026-03-21 20:55:53 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Silver/11053. 가장 긴 증가하는 부분 수열/Main.py
+++ BOJ/Silver/11053. 가장 긴 증가하는 부분 수열/Main.py
@@ -7,16 +7,14 @@ from collections import deque
 # 백준 11053 가장 긴 증가하는 부분 수열
 N = int(input())
 A = list(map(int, input().split()))
-cnt =0
-dp = [0]*(N+1)
 
-dp[1] = 1
+# 자기 자신을 포함하기 때문에 1
+dp = [1]*(N+1)
 
-for i in range(2, N+1):
-    if A[i-1] >= A[i-2]:
-        dp[i] = dp[i-1] + 1
-    else:
-        dp[i] = dp[i-1]
+for i in range(N):
+    for j in range(i):
+        if A[j] <= A[i]:
+            dp[i] = max(dp[i], dp[j]+1)
 
 
-print(dp[N])
+print(max(dp))
