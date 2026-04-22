ccdadc2b201cbef2a1f1551dd14b2012f7ec90a9
이영은 <yongeuni22@gmail.com>
2026-04-22 17:23:27 +0900

    [Gold III] Title: 게리맨더링, Time: -1 ms, Memory: -1 KB - DashHub

--- BOJ/Gold/17471. 게리맨더링/Main.py
+++ BOJ/Gold/17471. 게리맨더링/Main.py
@@ -1,63 +1,53 @@
 import sys
-# sys.stdin = open("input.txt", "r")
 input = sys.stdin.readline
 
 from collections import deque
 
-# 백준 17471 게리맨더링
-
-def dfs(idx, A):
-    global min_pop
-
-    # 종료 조건: 끝까지 갔다
-    if idx == N:
-        if 0 < len(A) < N:
-            B = [i for i in range(N) if i not in A]
-            # print('A:', A, 'B:', B)
-            if check(A) and check(B):
-                diff = abs(sum(population[i] for i in A) 
-                           - sum(population[i] for i in B))
-                min_pop = min(min_pop, diff)
-        return 
-
-    # A에 포함
-    dfs(idx+1, A + [idx])
-
-    # A에 포함 x
-    dfs(idx+1, A)
-
-def check(arr):
-    # 두 선거구가 각각 연결되어있는지 확인 
-    visited = set()
-    dq = deque([arr[0]])
-    visited.add(arr[0])
-
-    while dq:
-        now = dq.popleft()
-        
-        for next in graph[now]:
-            if next in arr and next not in visited:
-                visited.add(next)
-                dq.append(next)
-    return len(visited) == len(arr)
-    
-
-# 구역의 개수 N
+# 백준 게리멘더링
 N = int(input())
-
-# 구역의 인구 배열
 population = list(map(int, input().split()))
 
-graph = []
+arr = [list(map(int,input().split())) for _ in range(N)]
+min_result = float('inf')
 
-for i in range(N):
-    a = list(map(int, input().split()))
-    graph.append([x-1 for x in a[1:]])
+# 선거구를 2개로 나눔
+def dfs(now, A):
+    global min_result
+    
+    # 종료 조건:
+    if now == N:
+        if 0 < len(A) < N:
+            
+            # B 만들기
+            B = []
+            for i in range(N):
+                if i not in A:
+                    B.append(i)
+            
+            if check(A) and check(B):
+                result = abs(sum(population[i] for i in A) - sum(population[i] for i in B))
+                min_result = min(min_result, result)
+        return 
 
-min_pop = float('inf')
+    # 선거구 넣음
+    dfs(now+1, A+[now])
+    
+    # 안넣음
+    dfs(now+1, A)
+
+# group 내에 구역들이 모두 이어져있는지 체크 
+def check(group):
+    visited = set()
+    dq = deque([group[0]])
+    visited.add(group[0])
+    while dq:
+        now = dq.popleft()
+        for next in arr[now]:
+            if next in group and next not in visited:
+                visited.add(next)
+                dq.append(next)
+    return len(visited) == len(group)
 
 dfs(0, [])
-if min_pop == float('inf'):
-    print(-1)
-else:
-    print(min_pop)
+
+print(min_result)
